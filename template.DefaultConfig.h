9249d9887a2d158a0089cda581a66bf4143cccb3
PROJECT-FATE\trami <tramircz@gmail.com>
2018-02-13 16:41:26 +0100

    add template for default config file

---
+++ template.DefaultConfig.h
@@ -0,0 +1,14 @@
+// -- Default Wifi config is used after flash -- //
+
+const char* dc_ssid = "";
+const char* dc_password = "";
+int         dc_dhcp = 1;
+IPAddress   dc_ip = IPAddress(192, 168, 100, 26);
+IPAddress   dc_netmask = IPAddress(255, 255, 255, 0);
+IPAddress   dc_gateway = IPAddress(192, 168, 100, 1);
+IPAddress   dc_dns = IPAddress(192, 168, 100, 1);
+const char* dc_ntpServerName = "pool.ntp.org";
+int         dc_updateNTPTimeEvery = 15;
+int         dc_timezone = 10;
+int         dc_daylight = 1;
+const char* dc_deviceName = "Switch";
